86b82091ee4ece6bbf1bd10940f5f2540826e500
mingmoth <mingmoth@gmail.com>
2022-03-16 15:52:29 +0800

    proj init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
+import React, { useState, useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [etheurPrice, setEtheurPrice] = useState()
+  const [lastPrice, setLastPrice] = useState()
+
+  useEffect(() => {
+    let ws = new WebSocket('wss://stream.binance.com:9443/ws/etheur@trade')
+    ws.onmessage = (event) => {
+      setLastPrice(etheurPrice)
+      let etheurData = JSON.parse(event.data)
+      setEtheurPrice(parseFloat(etheurData.p).toFixed(2))
+    }
+  }, [etheurPrice, lastPrice])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ETH: <span style={{ color: !etheurPrice || etheurPrice===lastPrice? 'white': etheurPrice>lastPrice? 'green' : 'red' }}>{etheurPrice}</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
